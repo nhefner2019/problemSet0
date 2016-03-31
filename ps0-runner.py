b5f8d43ffd2abe13d10f090b09703c94461ff56c
Nate Hefner <natehefner@Nathans-Air.fios-router.home>
2016-03-30 21:05:05 -0400

    added test cases for problem 1

--- ps0-runner.py
+++ ps0-runner.py
@@ -3,3 +3,8 @@ import ps0
 #Test cases for Problem 0
 print("The number {} is even: {}.".format(4,ps0.even_odd(4)))
 print("The number {} is even: {}.".format(5,ps0.even_odd(5)))
+
+#Test cases for Problem 1
+print("Your number is {}. There is {} digits in {}.".format(1,ps0.number_digits(1),1))
+print("Your number is {}. There are {} digits in {}.".format(55,ps0.number_digits(55),55))
+print("Your number is {}. There are {} digits in {}.".format(4875,ps0.number_digits(4875),4875))
