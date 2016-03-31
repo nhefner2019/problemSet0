1bc5ca12b4c4d5722ce77361ce0352c450478cb4
Nate Hefner <natehefner@Nathans-Air.fios-router.home>
2016-03-30 20:56:43 -0400

    problems 0-6 without main programs

--- ps0.py
+++ ps0.py
@@ -2,55 +2,35 @@ from math import factorial
 
 #Problem 0
 
-'''def even_odd(number):
+def even_odd(number):
 	
 	if number % 2 == 0:
 		return True
 	else:
 		return False
-		
-#----------Main Program-------------
-number = int(raw_input("What is your number? "))
-
-numb = even_odd(number)
-
-print(numb)'''
 
 
 #Problem 1
 
-'''def number_digits(number):
+def number_digits(number):
 	
 	digits = len(number)
 	
 	return digits
-	
-#------------main program------------
-number = raw_input("What is your number? "))
-
-numb = number_digits(number)
-
-print("Your number has {} digits.".format(numb))'''
 
 
 #Problem 2
 
-'''def sum_digits(number):
+def sum_digits(number):
 	sum = 0
 	for digit in number:
 		sum += int(digit)
 	return sum
 		
-#--------------Main Program----------------
-
-number = raw_input("What is your number? ")
-sum = sum_digits(number)
-print("The sum of your digits is {}." .format(sum))'''
-		
 
 #Problem 3
 
-'''def sum_less_ints(number):
+def sum_less_ints(number):
 	sum = 0
 	ints = range(number)
 	
@@ -59,49 +39,29 @@ print("The sum of your digits is {}." .format(sum))'''
 	
 	return sum
 	
-#-------------Main Program------------
-number = int(raw_input("What is your number? "))
-
-sum = sum_less_ints(number)
-
-print("The sum of the integers less than {} is {}.".format(number, sum))'''
 
 #Problem 4
 
-'''def number_factorial(number):
+def number_factorial(number):
 	
 	factor = factorial(number)
 	
 	return factor
 
-#--------Main Program------------
-number = int(raw_input("What is your number? "))
-
-factor = number_factorial(number)
-
-print("The factorial of {} is {}." .format(number, factor))'''
 
 
 #Problem 5
 
-'''def possible_factors(dividend, divisor):
+def possible_factors(dividend, divisor):
 
 	if dividend % divisor == 0:
 		return True
 	else:
 		return False
 		
-#-------------Main Program-------------
-dividend = int(raw_input("What will be your dividend? "))
-divisor = int(raw_input("What will be your divisor? "))
-
-quotient = possible_factors(dividend, divisor)
-
-print(quotient)'''
-
 #Problem 6
 
-'''def prime_number(number):
+def prime_number(number):
 
 	numberRange = range(2, number)
 	
@@ -111,12 +71,8 @@ print(quotient)'''
 			
 		return False
 
-#-----------Main Program--------------
-number = int(raw_input("What is your number? "))
 
-result = prime_number(number)
-
-print(result)'''
+	
 
 
 
